8a44758af20912925df9cf80c42be46f69919136
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-07 16:41:00 +0200

    Add project files.

---
+++ Entry_to_C++/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+
+{
+	const string BLUE = "\033[36m";
+	const string MAGENTA = "\033[35m";
+	const string GRAY = "\033[90m";
+	const string RESET = "\033[0m";
+
+	cout << BLUE << "Lose yourself - Eminen\n" << RESET << endl;
+
+	cout << GRAY << "[Intro]" << RESET << endl;
+	cout << MAGENTA << "Look, if you had one shot, or one opportunity" << endl;
+	cout << "To seize everything you ever wanted - One moment" << endl;
+	cout << "Would you capture it or just let it slip ?" << endl;
+	cout << "Yo\n" << RESET << endl;
+
+	cout << GRAY << "[Verse 1]" << RESET << endl;
+	cout << MAGENTA << "His palms are sweaty, knees weak, arms are heavy" << endl;
+	cout << "There's vomit on his sweater already, mom's spaghetti" << endl;
+	cout << "He's nervous, but on the surface he looks calm and ready" << endl;
+	cout << "To drop bombs, but he keeps on forgettin" << endl;
+	cout << "What he wrote down, the whole crowd goes so loud" << endl;
+	cout << "He opens his mouth, but the words won't come out" << endl;
+	cout << "He's chokin, how everybody's jokin now" << endl;
+	cout << "The clock's run out, time's up over, bloah!" << endl;
+	cout << "Snap back to reality, Oh there goes gravity" << endl;
+	cout << "Oh, there goes Rabbit, he choked" << endl;
+	cout << "He's so mad, but he won't give up that" << endl;
+	cout << "Easy, no" << endl;
+	cout << "He won't have it , he knows his whole back's to these ropes" << endl;
+	cout << "It don't matter, he's dope" << endl;
+	cout << "He knows that, but he's broke" << endl;
+	cout << "He's so stagnant that he knows" << endl;
+	cout << "When he goes back to his mobile home, that's when it's" << endl;
+	cout << "Back to the lab again yo" << endl;
+	cout << "This whole rap shit" << endl;
+	cout << "He better go capture this moment and hope it don't pass him" << RESET << endl;
+
+	cout << GRAY << "\n[Chorus]" << RESET << endl;
+	cout << MAGENTA << "You better lose yourself in the music" << endl;
+	cout << "The moment, you own it, you better never let it go(Go)" << endl;
+	cout << "You only get one shot, do not miss your chance to blow" << endl;
+	cout << "This opportunity comes once in a lifetime, yo" << endl;
+	cout << "You better lose yourself in the music" << endl;
+	cout << "The moment, you own it, you better never let it go(Go)" << endl;
+	cout << "You only get one shot, do not miss your chance to blow" << endl;
+	cout << "This opportunity comes once in a lifetime, yo" << endl;
+	cout << "You better" << RESET << endl; cout << GRAY << "\n[Verse 2]" << RESET << endl;
+	cout << MAGENTA << "His soul's escaping through this hole that is gaping" << endl;
+	cout << "This world is mine for the taking, make me king" << endl;
+	cout << "As we move toward a new world order" << endl;
+	cout << "A normal life is boring, but superstardom's" << endl;
+	cout << "Close to post - mortem, it only grows harder" << endl;
+	cout << "Homie grows hotter, he blows, it's all over" << endl;
+	cout << "These hoes is all on him, coast - to - coast shows" << endl;
+	cout << "He's known as the Globetrotter, lonely roads" << endl;
+	cout << "God only knows he's grown farther from home, he's no father" << endl;
+	cout << "He goes home and barely knows his own daughter" << endl;
+	cout << "But hold your nose 'cause here goes the cold water" << endl;
+	cout << "These hoes don't want him no mo', he's cold product" << endl;
+	cout << "They moved on to the next schmoe who flows" << endl;
+	cout << "He nose - dove and sold nada, and so the soap opera" << endl;
+	cout << "Is told, it unfolds, I suppose it's old, partner" << endl;
+	cout << "But the beat goes on, da - da - dom, da - dom, dah - dah - dah - dah" << RESET << endl;
+
+	cout << GRAY << "\n[Chorus]" << RESET << endl;
+	cout << MAGENTA << "You better lose yourself in the music" << endl;
+	cout << "The moment, you own it, you better never let it go(Go)" << endl;
+	cout << "You only get one shot, do not miss your chance to blow" << endl;
+	cout << "This opportunity comes once in a lifetime, yo" << endl;
+	cout << "You better lose yourself in the music" << endl;
+	cout << "The moment, you own it, you better never let it go(Go)" << endl;
+	cout << "You only get one shot, do not miss your chance to blow" << endl;
+	cout << "This opportunity comes once in a lifetime, yo" << endl;
+	cout << "You better" << RESET << endl;
+
+	_getch();
+	return 0;
+
+}
